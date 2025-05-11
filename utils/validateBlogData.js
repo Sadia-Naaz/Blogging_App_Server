4a6365f857255038b5dcd18ee93fb3d48e671b55
Sadia Naaz <sadianaaz110@.com>
2025-05-11 21:53:42 +0530

    increased the size of blog

--- utils/validateBlogData.js
+++ utils/validateBlogData.js
@@ -1,8 +1,8 @@
 const validateBlogData = ({title,textBody})=>{
     return new Promise ((res,rej)=>{
         if(!title ||!textBody) return rej('Please fill in all fields');
-        if(title.length <3 ||title.length>100) return rej("character length should be between 3-100 characters of Title");
-        if(textBody.length < 3 || textBody.length>1000) return rej("character length should be between 3-1000 characters of Title")
+        if(title.length <3 ||title.length>100) return rej("character length should be between 3-500 characters of Title");
+        if(textBody.length < 3 || textBody.length>1000) return rej("character length of blog should be between 3-2000 characters")
         if(typeof title !== "string") return rej("text Type is not string");
         if(typeof textBody !== "string") return rej("text Type is not string");
         res();
