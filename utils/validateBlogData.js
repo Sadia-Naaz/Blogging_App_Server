a6b9b17a42ab9797aea9f47dc3608d679d8d48d2
Sadia Naaz <sadianaaz110@.com>
2025-05-11 22:00:49 +0530

    new commit

--- utils/validateBlogData.js
+++ utils/validateBlogData.js
@@ -1,7 +1,7 @@
 const validateBlogData = ({title,textBody})=>{
     return new Promise ((res,rej)=>{
         if(!title ||!textBody) return rej('Please fill in all fields');
-        if(title.length <3 ||title.length>100) return rej("character length should be between 3-500 characters of Title");
+        if(title.length <3 ||title.length>500) return rej("character length should be between 3-500 characters of Title");
         if(textBody.length < 3 || textBody.length>2000) return rej("character length of blog should be between 3-2000 characters")
         if(typeof title !== "string") return rej("text Type is not string");
         if(typeof textBody !== "string") return rej("text Type is not string");
